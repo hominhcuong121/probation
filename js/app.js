6f0db4f54c7c66567594e0dc2dc047401e08e772
homin <hominhcuong121@gmail.com>
2018-05-03 00:26:16 +0700

    Completed All functions

--- js/app.js
+++ js/app.js
@@ -1,5 +1,29 @@
-var myApp = angualr.module("myApp", []);
+var myApp = angular.module("myApp", []);
 
 myApp.controller("myController", function($scope){
-    console.log("hihi");
+
+    // Mock Data
+    $scope.employees = [
+        {fullname: "Hồ Minh Cường", shortname: "cho38"},
+        {fullname: "Trần Thị Kim Dung", shortname: "tdung8"},
+        {fullname: "Vương Chí Phát", shortname: "vphat15"}
+    ];
+
+    // Add new employee
+    $scope.newEmployee = {};
+    $scope.saveEmployee = function() {
+        $scope.employees.push($scope.newEmployee);
+        $scope.newEmployee = {};
+    }
+
+    // Select employee
+    $scope.clickedEmployee = {};
+    $scope.selectEmployee = function(employee) {
+        $scope.clickedEmployee = employee;
+    }
+
+    // Delete employee
+    $scope.deleteEmployee = function() {
+        $scope.employees.splice($scope.employees.indexOf($scope.clickedEmployee), 1);
+    }
 });
