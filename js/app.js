9e808b595c912e45482e6d4c0c8afbf5ec501ec8
homin <hominhcuong121@gmail.com>
2018-05-08 23:02:23 +0700

    2nd Exercise Complted

--- js/app.js
+++ js/app.js
@@ -4,10 +4,12 @@ myApp.controller("myController", function($scope){
 
     // Mock Data
     $scope.employees = [
-        {fullname: "Hồ Minh Cường", shortname: "cho38"},
-        {fullname: "Trần Thị Kim Dung", shortname: "tdung8"},
-        {fullname: "Vương Chí Phát", shortname: "vphat15"}
+        {fullname: "Hồ Minh Cường", shortname: "cho38", maritalStatus: "Single", SpouseName: ''},
+        {fullname: "Trần Thị Kim Dung", shortname: "tdung8", maritalStatus: "Married", SpouseName: 'Phat'},
+        {fullname: "Vương Chí Phát", shortname: "vphat15", maritalStatus: "Married", SpouseName: 'Dung'}
     ];
+    $scope.maritalStatus = ["Married", "Single"]
+
 
     // Add new employee
     $scope.newEmployee = {};
